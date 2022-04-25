a330734f6288244357f70233ae31c76e0e2cf4c5
Clovis Chakrian <clovischakriandev@gmail.com>
2022-04-25 19:56:26 -0300

    deleted useless files

--- dist/routes/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const admins_1 = __importDefault(require("./admins"));
-const products_1 = __importDefault(require("./products"));
-const routes = [admins_1.default, products_1.default];
-exports.default = routes;
