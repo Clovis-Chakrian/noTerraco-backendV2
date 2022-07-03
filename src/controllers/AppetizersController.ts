816c18b8d74258c371527067f80f0ac552dc1488
Clóvis Chakrian <clovischakriandev@gmail.com>
2022-07-03 17:06:41 -0300

    Finished create and index functions

--- src/controllers/AppetizersController.ts
+++ src/controllers/AppetizersController.ts
@@ -5,13 +5,83 @@ import prismaClient from '../database/prismaClient';
 import removeSpecialCharacters from '../utils/removeSpecialCharacters';
 
 export default {
-  async create(request: Request, response: Response) {},
+  async create(request: Request, response: Response) {
+    prismaClient.$connect();
+    const {
+      name,
+      description,
+      image,
+      price,
+      availability,
+    } = request.body;
 
-  async index(request: Request, response: Response) {},
+    let appetizersSchema = object({
+      name: string().required(),
+      description: string().required(),
+      image: string().required(),
+      price: number().required(),
+      availability: boolean().required(),
+    });
 
-  async show(request: Request, response: Response) {},
+    const base64 = image.replace(/^data:image\/jpeg;base64,/, "");
+    const fileName = `${removeSpecialCharacters(name.replaceAll(' ', '_'))}`;
 
-  async update(request: Request, response: Response) {},
+    const appetizer = {
+      name,
+      description,
+      imageURL: fileName,
+      price,
+      availability,
+      updateTimes: 1
+    };
 
-  async delete(request: Request, response: Response) {},
+    await appetizersSchema.validate(appetizer, {
+      abortEarly: false
+    }).then(async () => {
+      uploadImage(base64, `${fileName}_${appetizer.updateTimes}`, '/entradinhas')
+
+      await prismaClient.appetizers.create({
+        data: appetizer
+      });
+
+      return response.status(201).json({ message: 'Entradinha adicionada com sucesso!' })
+    }).catch((err: ValidationError) => {
+      return response.status(400).json(err.errors);
+    });
+  },
+
+  async index(request: Request, response: Response) {
+    await prismaClient.$connect();
+    const { name } = request.query as {
+      name: string
+    };
+
+    if (name) {
+      try {
+        const appetizer = await prismaClient.appetizers.findMany({
+          where: { name }
+        });
+
+        return response.status(200).json({ appetizer })
+      } catch (error) {
+        console.log(error);
+        return response.status(500).json({ message: 'erro interno do servidor!' });
+      }
+    } else {
+      try {
+        const appetizers = await prismaClient.appetizers.findMany();
+
+        return response.status(200).json({appetizers});
+      } catch (error) {
+        console.log(error);
+        return response.status(500).json({ message: 'Erro interno do servidor' });
+      }
+    }
+  },
+
+  async show(request: Request, response: Response) { },
+
+  async update(request: Request, response: Response) { },
+
+  async delete(request: Request, response: Response) { },
 }
